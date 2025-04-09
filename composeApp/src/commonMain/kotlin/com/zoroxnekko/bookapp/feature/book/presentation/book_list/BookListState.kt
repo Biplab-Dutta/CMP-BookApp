393c6915bb597d7a280345f28ffd9ebc60af517c
Biplab Dutta <biplabdutta860@gmail.com>
2025-04-09 23:53:28 +0545

    add a BookListState class that holds state information for book list page

---
+++ composeApp/src/commonMain/kotlin/com/zoroxnekko/bookapp/feature/book/presentation/book_list/BookListState.kt
@@ -0,0 +1,13 @@
+package com.zoroxnekko.bookapp.feature.book.presentation.book_list
+
+import com.zoroxnekko.bookapp.core.presentation.UiText
+import com.zoroxnekko.bookapp.feature.book.domain.Book
+
+data class BookListState(
+    val searchQuery: String = "Kotlin",
+    val searchResults: List<Book> = emptyList(),
+    val favoriteBooks: List<Book> = emptyList(),
+    val isLoading: Boolean = false,
+    val selectedTabIndex: Int = 0,
+    val errorMessage: UiText? = null,
+)
