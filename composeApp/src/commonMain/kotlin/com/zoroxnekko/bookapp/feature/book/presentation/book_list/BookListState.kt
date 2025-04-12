e1d712d94554a08eda1070bc8a8ca7ae78708d98
Biplab Dutta <biplabdutta860@gmail.com>
2025-04-12 23:33:12 +0545

    add dummy book list for demonstration purpose

--- composeApp/src/commonMain/kotlin/com/zoroxnekko/bookapp/feature/book/presentation/book_list/BookListState.kt
+++ composeApp/src/commonMain/kotlin/com/zoroxnekko/bookapp/feature/book/presentation/book_list/BookListState.kt
@@ -5,9 +5,25 @@ import com.zoroxnekko.bookapp.feature.book.domain.Book
 
 data class BookListState(
     val searchQuery: String = "Kotlin",
-    val searchResults: List<Book> = emptyList(),
+    val searchResults: List<Book> = dummyBooks,
     val favoriteBooks: List<Book> = emptyList(),
     val isLoading: Boolean = false,
     val selectedTabIndex: Int = 0,
     val errorMessage: UiText? = null,
 )
+
+val dummyBooks = (1..100).map {
+    Book(
+        id = it.toString(),
+        title = "Book $it",
+        imageUrl = "https://icons.veryicon.com/png/o/business/colorful-office-icons/book-52.png",
+        authors = listOf("Philipp Lackner"),
+        description = "Description $it",
+        languages = emptyList(),
+        firstPublishYear = null,
+        averageRating = 4.67854,
+        ratingCount = 5,
+        numPages = 100,
+        numEditions = 3
+    )
+}
