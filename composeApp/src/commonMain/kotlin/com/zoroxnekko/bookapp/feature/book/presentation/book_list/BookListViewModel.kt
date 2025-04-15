f9b5559ff05d08d1ff0cfe866dc10e2060c328f4
Biplab Dutta <biplabdutta860@gmail.com>
2025-04-15 21:31:16 +0545

    add viewmodel logic for search functionality

--- composeApp/src/commonMain/kotlin/com/zoroxnekko/bookapp/feature/book/presentation/book_list/BookListViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/zoroxnekko/bookapp/feature/book/presentation/book_list/BookListViewModel.kt
@@ -1,13 +1,44 @@
 package com.zoroxnekko.bookapp.feature.book.presentation.book_list
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.zoroxnekko.bookapp.core.domain.onError
+import com.zoroxnekko.bookapp.core.domain.onSuccess
+import com.zoroxnekko.bookapp.core.presentation.toUiText
+import com.zoroxnekko.bookapp.feature.book.domain.Book
+import com.zoroxnekko.bookapp.feature.book.domain.BookRepository
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
-class BookListViewModel : ViewModel() {
+class BookListViewModel(
+    private val bookRepository: BookRepository
+) : ViewModel() {
+    private var cachedBooks = emptyList<Book>()
+    private var searchJob: Job? = null
+
     private val _state = MutableStateFlow(BookListState())
-    val state = _state.asStateFlow()
+    val state = _state
+        .onStart {
+            if (cachedBooks.isEmpty()) {
+                observerSearchQuery()
+            }
+        }
+        .stateIn(
+            viewModelScope,
+            SharingStarted.WhileSubscribed(5000L),
+            BookListState()
+        )
 
     fun onAction(action: BookListAction) {
         when (action) {
@@ -21,5 +52,55 @@ class BookListViewModel : ViewModel() {
 
             is BookListAction.OnTabSelected -> {}
         }
+    }
+
+    @OptIn(FlowPreview::class)
+    private fun observerSearchQuery() {
+        state
+            .map { it.searchQuery }
+            .distinctUntilChanged()
+            .debounce { 500L }
+            .onEach { query ->
+                when {
+                    query.isBlank() -> {
+                        _state.update {
+                            it.copy(
+                                errorMessage = null,
+                                searchResults = cachedBooks
+                            )
+                        }
+                    }
+
+                    query.length >= 2 -> {
+                        searchJob?.cancel()
+                        searchJob = searchBooks(query)
+                    }
+                }
+            }.launchIn(viewModelScope)
+    }
+
+    private fun searchBooks(query: String): Job = viewModelScope.launch {
+        _state.update {
+            it.copy(isLoading = true)
+        }
+        bookRepository.searchBooks(query)
+            .onSuccess { searchResults ->
+                _state.update {
+                    it.copy(
+                        isLoading = false,
+                        errorMessage = null,
+                        searchResults = searchResults
+                    )
+                }
+            }
+            .onError { error ->
+                _state.update {
+                    it.copy(
+                        searchResults = emptyList(),
+                        isLoading = false,
+                        errorMessage = error.toUiText()
+                    )
+                }
+            }
     }
 }
