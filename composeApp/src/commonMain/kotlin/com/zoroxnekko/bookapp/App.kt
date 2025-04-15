56be07d800490962cba79bb7faf38c06db0f2a86
Biplab Dutta <biplabdutta860@gmail.com>
2025-04-15 21:32:30 +0545

    pass BookRepoImpl instance inside of the BookListViewModel in App composable as a temporary workaround to get the app up and running

--- composeApp/src/commonMain/kotlin/com/zoroxnekko/bookapp/App.kt
+++ composeApp/src/commonMain/kotlin/com/zoroxnekko/bookapp/App.kt
@@ -2,13 +2,25 @@ package com.zoroxnekko.bookapp
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import com.zoroxnekko.bookapp.core.data.HttpClientFactory
+import com.zoroxnekko.bookapp.feature.book.data.network.RemoteBookDataSource
+import com.zoroxnekko.bookapp.feature.book.data.repository.BookRepositoryImpl
 import com.zoroxnekko.bookapp.feature.book.presentation.book_list.BookListScreenRoot
 import com.zoroxnekko.bookapp.feature.book.presentation.book_list.BookListViewModel
+import io.ktor.client.engine.HttpClientEngine
 
 @Composable
-fun App() {
+fun App(engine: HttpClientEngine) {
     BookListScreenRoot(
-        viewModel = remember { BookListViewModel() },
+        viewModel = remember {
+            BookListViewModel(
+                BookRepositoryImpl(
+                    RemoteBookDataSource(
+                        HttpClientFactory.create(engine)
+                    ),
+                )
+            )
+        },
         onBookClick = {}
     )
 }
