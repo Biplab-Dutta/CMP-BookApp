e092d44b42c5fdaf4bbae95f4c207fb1f1e96dbe
Biplab Dutta <biplabdutta860@gmail.com>
2025-04-15 21:33:17 +0545

    pass network client for iOS app in MainViewController as a temporary workaround to get the up and running

--- composeApp/src/iosMain/kotlin/com/zoroxnekko/bookapp/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/zoroxnekko/bookapp/MainViewController.kt
@@ -1,5 +1,9 @@
 package com.zoroxnekko.bookapp
 
+import androidx.compose.runtime.remember
 import androidx.compose.ui.window.ComposeUIViewController
+import io.ktor.client.engine.darwin.Darwin
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController {
+    App(engine = remember { Darwin.create() })
+}
