3e42e75068de7e1b0092ef9f143a1373a9bef955
Biplab Dutta <biplabdutta860@gmail.com>
2025-04-18 22:50:52 +0545

    remove dummyBooks from BookListState

--- composeApp/src/commonMain/kotlin/com/zoroxnekko/bookapp/feature/book/presentation/book_list/BookListState.kt
+++ composeApp/src/commonMain/kotlin/com/zoroxnekko/bookapp/feature/book/presentation/book_list/BookListState.kt
@@ -5,25 +5,9 @@ import com.zoroxnekko.bookapp.feature.book.domain.Book
 
 data class BookListState(
     val searchQuery: String = "Kotlin",
-    val searchResults: List<Book> = dummyBooks,
+    val searchResults: List<Book> = emptyList(),
     val favoriteBooks: List<Book> = emptyList(),
-    val isLoading: Boolean = false,
+    val isLoading: Boolean = true,
     val selectedTabIndex: Int = 0,
     val errorMessage: UiText? = null,
 )
-
-val dummyBooks = (1..100).map {
-    Book(
-        id = it.toString(),
-        title = "Book $it",
-        imageUrl = "https://icons.veryicon.com/png/o/business/colorful-office-icons/book-52.png",
-        authors = listOf("Philipp Lackner"),
-        description = "Description $it",
-        languages = emptyList(),
-        firstPublishYear = null,
-        averageRating = 4.67854,
-        ratingCount = 5,
-        numPages = 100,
-        numEditions = 3
-    )
-}
