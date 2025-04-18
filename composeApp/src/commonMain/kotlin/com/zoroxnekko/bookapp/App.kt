66eeb1d83faa616aaec2412cf2f937bf697aa32f
Biplab Dutta <biplabdutta860@gmail.com>
2025-04-18 22:33:39 +0545

    remove manually injected DI

--- composeApp/src/commonMain/kotlin/com/zoroxnekko/bookapp/App.kt
+++ composeApp/src/commonMain/kotlin/com/zoroxnekko/bookapp/App.kt
@@ -1,26 +1,15 @@
 package com.zoroxnekko.bookapp
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import com.zoroxnekko.bookapp.core.data.HttpClientFactory
-import com.zoroxnekko.bookapp.feature.book.data.network.RemoteBookDataSource
-import com.zoroxnekko.bookapp.feature.book.data.repository.BookRepositoryImpl
 import com.zoroxnekko.bookapp.feature.book.presentation.book_list.BookListScreenRoot
 import com.zoroxnekko.bookapp.feature.book.presentation.book_list.BookListViewModel
-import io.ktor.client.engine.HttpClientEngine
+import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
-fun App(engine: HttpClientEngine) {
+fun App() {
+    val viewModel = koinViewModel<BookListViewModel>()
     BookListScreenRoot(
-        viewModel = remember {
-            BookListViewModel(
-                BookRepositoryImpl(
-                    RemoteBookDataSource(
-                        HttpClientFactory.create(engine)
-                    ),
-                )
-            )
-        },
+        viewModel = viewModel,
         onBookClick = {}
     )
 }
--- composeApp/src/iosMain/kotlin/com/zoroxnekko/bookapp/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/zoroxnekko/bookapp/MainViewController.kt
@@ -1,9 +1,12 @@
 package com.zoroxnekko.bookapp
 
-import androidx.compose.runtime.remember
 import androidx.compose.ui.window.ComposeUIViewController
-import io.ktor.client.engine.darwin.Darwin
+import com.zoroxnekko.bookapp.di.initKoin
 
-fun MainViewController() = ComposeUIViewController {
-    App(engine = remember { Darwin.create() })
+fun MainViewController() = ComposeUIViewController(
+    configure = {
+        initKoin()
+    }
+) {
+    App()
 }
