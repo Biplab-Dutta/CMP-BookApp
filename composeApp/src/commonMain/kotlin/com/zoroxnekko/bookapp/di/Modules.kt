c0daf880be74d52d17b6c1d754535ea590bfa0f3
Biplab Dutta <biplabdutta860@gmail.com>
2025-04-18 22:35:02 +0545

    add koin modules for DI

---
+++ composeApp/src/commonMain/kotlin/com/zoroxnekko/bookapp/di/Modules.kt
@@ -0,0 +1,22 @@
+package com.zoroxnekko.bookapp.di
+
+import com.zoroxnekko.bookapp.core.data.HttpClientFactory
+import com.zoroxnekko.bookapp.feature.book.data.network.RemoteBookDataSource
+import com.zoroxnekko.bookapp.feature.book.data.repository.BookRepositoryImpl
+import com.zoroxnekko.bookapp.feature.book.domain.BookRepository
+import com.zoroxnekko.bookapp.feature.book.presentation.book_list.BookListViewModel
+import org.koin.core.module.Module
+import org.koin.core.module.dsl.singleOf
+import org.koin.core.module.dsl.viewModelOf
+import org.koin.dsl.bind
+import org.koin.dsl.module
+
+expect val platformModule: Module
+
+val sharedModule = module {
+    single { HttpClientFactory.create(get()) }
+    singleOf(::RemoteBookDataSource)
+    singleOf(::BookRepositoryImpl).bind<BookRepository>()
+
+    viewModelOf(::BookListViewModel)
+}
