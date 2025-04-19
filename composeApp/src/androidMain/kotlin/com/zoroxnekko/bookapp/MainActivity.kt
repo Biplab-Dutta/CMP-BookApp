5a088a2e1d47e05624bb7d83a559de5a3d13cdea
Biplab Dutta <biplabdutta860@gmail.com>
2025-04-19 07:20:12 +0545

    add additional import in MainActivity.kt

--- composeApp/src/androidMain/kotlin/com/zoroxnekko/bookapp/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/zoroxnekko/bookapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.zoroxnekko.bookapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.zoroxnekko.app.App
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
