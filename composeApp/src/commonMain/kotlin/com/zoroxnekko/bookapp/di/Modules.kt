51c329ee93e24ebc5ba3aee9f4ba49b85c127ddb
Biplab Dutta <biplabdutta860@gmail.com>
2025-04-19 07:21:34 +0545

    add a module declaration for SelectedBookViewModel

--- composeApp/src/commonMain/kotlin/com/zoroxnekko/bookapp/di/Modules.kt
+++ composeApp/src/commonMain/kotlin/com/zoroxnekko/bookapp/di/Modules.kt
@@ -4,6 +4,7 @@ import com.zoroxnekko.bookapp.core.data.HttpClientFactory
 import com.zoroxnekko.bookapp.feature.book.data.network.RemoteBookDataSource
 import com.zoroxnekko.bookapp.feature.book.data.repository.BookRepositoryImpl
 import com.zoroxnekko.bookapp.feature.book.domain.BookRepository
+import com.zoroxnekko.bookapp.feature.book.presentation.SelectedBookViewModel
 import com.zoroxnekko.bookapp.feature.book.presentation.book_list.BookListViewModel
 import org.koin.core.module.Module
 import org.koin.core.module.dsl.singleOf
@@ -19,4 +20,5 @@ val sharedModule = module {
     singleOf(::BookRepositoryImpl).bind<BookRepository>()
 
     viewModelOf(::BookListViewModel)
+    viewModelOf(::SelectedBookViewModel)
 }
