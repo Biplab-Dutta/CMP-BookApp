bc070d86dcdb7012f96f7e6585dc1512fe9f1bee
Biplab Dutta <biplabdutta860@gmail.com>
2025-04-19 07:19:47 +0545

    add a common viewmodel that holds book information for the detail page

---
+++ composeApp/src/commonMain/kotlin/com/zoroxnekko/bookapp/feature/book/presentation/SelectedBookViewModel.kt
@@ -0,0 +1,15 @@
+package com.zoroxnekko.bookapp.feature.book.presentation
+
+import androidx.lifecycle.ViewModel
+import com.zoroxnekko.bookapp.feature.book.domain.Book
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class SelectedBookViewModel : ViewModel() {
+    private val _selectedBook = MutableStateFlow<Book?>(null)
+    val selectedBook = _selectedBook.asStateFlow()
+
+    fun onSelectBook(book: Book?) {
+        _selectedBook.value = book
+    }
+}
