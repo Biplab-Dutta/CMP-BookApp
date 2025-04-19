118d1f7aa2c66bf70006791b193636df57e946d1
Biplab Dutta <biplabdutta860@gmail.com>
2025-04-19 07:20:58 +0545

    add additional import in MainViewController.kt

--- composeApp/src/iosMain/kotlin/com/zoroxnekko/bookapp/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/zoroxnekko/bookapp/MainViewController.kt
@@ -1,6 +1,7 @@
 package com.zoroxnekko.bookapp
 
 import androidx.compose.ui.window.ComposeUIViewController
+import com.zoroxnekko.app.App
 import com.zoroxnekko.bookapp.di.initKoin
 
 fun MainViewController() = ComposeUIViewController(
